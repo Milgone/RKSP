adbb3245d81c736931a67bbe2c86e488742198f5
Milgone <145690168+Milgone@users.noreply.github.com>
2025-12-08 16:03:06 +0000

    1 лаба начало

---
+++ Kondrya/src/main/java/ru/rksp/Kondrya/KondryaApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Kondrya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KondryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KondryaApplication.class, args);
+	}
+
+}
